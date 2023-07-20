903a73b2a7fadbbed89801b3582d7b5be421f7b3
huchangjie <huchangjie@live.cn>
2023-07-20 14:24:38 +0800

    1.Container容器实例

--- HucjTest/flutter_module/lib/stack/LayoutBuilderDemo.dart
+++ HucjTest/flutter_module/lib/stack/LayoutBuilderDemo.dart
@@ -90,6 +90,38 @@ class LayoutBuilderRoute extends StatelessWidget {
                           height: 1)),
                 ),
               ),
+            ),
+
+            const SizedBox(
+              height: 20,
+            ),
+
+            Container(
+              margin: const EdgeInsets.only(top: 50.0, left: 120.0),
+              constraints:
+                  const BoxConstraints.tightFor(width: 200.0, height: 150.0),
+              //背景装饰
+              decoration: const BoxDecoration(
+                  //背景径向渐变
+                  gradient: RadialGradient(
+                    colors: [Colors.red, Colors.orange],
+                    center: Alignment.topLeft,
+                    radius: .98,
+                  ),
+
+                  boxShadow: [
+                    //卡片阴影
+                    BoxShadow(
+                        color: Colors.black54,
+                        offset: Offset(2.0, 2.0),
+                        blurRadius: 4.0),
+                  ],),
+              transform: Matrix4.rotationZ(.2),
+              alignment: Alignment.center,
+              child: const Text(
+                '5.21',
+                style: TextStyle(color: Colors.white, fontSize: 40.0),
+              ),
             )
           ],
         ));
